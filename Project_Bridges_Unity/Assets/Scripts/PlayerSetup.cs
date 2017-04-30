5647e716be5bc759a32f47a6fd23ea41b6abc8f0
Stef Colruyt <stef.col@hotmail.com>
2017-04-30 22:20:57 +0200

    Cleaned up player generation.

--- Project_Bridges_Unity/Assets/Scripts/PlayerSetup.cs
+++ Project_Bridges_Unity/Assets/Scripts/PlayerSetup.cs
@@ -5,7 +5,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [ExecuteInEditMode]
-public class PlayerSetup : MonoBehaviour {
+public class PlayerSetup : MonoBehaviour
+{
 
     public bool generate;
     public bool update;
@@ -14,35 +15,53 @@ public class PlayerSetup : MonoBehaviour {
 
     public List<Player> players;
 
-    void Start() {
-        
+    void Start()
+    {
+
     }
 
-    void Update() {
-        if (generate) {
-            players.Clear();
-            for (int i = 1; i <= amountOfPlayers; i++) {
-                players.Add(new Player(i, InputMode.Keyboard));
-            }
+    void Update()
+    {
+        if (generate)
+        {
+            GeneratePlayers();
             generate = false;
         }
 
-        if (update) {
-            foreach (Player p in players) {
-                if (p.inputMode != p.inputModeOld) {
-                    p.inputModeOld = p.inputMode;
-                    p.horizontalInput = "Player_" + p.playerNum + "_" + p.inputMode.ToString() + "_Horizontal";
-                    p.verticalInput = "Player_" + p.playerNum + "_" + p.inputMode.ToString() + "_Vertical";
-                    p.button = "Player_" + p.playerNum + "_" + p.inputMode.ToString() + "_Button";
-                }
-            }
+        if (update)
+        {
+            UpdatePlayer();
             update = false;
+        }
+    }
+
+    void GeneratePlayers()
+    {
+        players.Clear();
+        for (int i = 0; i < amountOfPlayers; i++)
+        {
+            players.Add(new Player(i + 1, InputMode.Keyboard));
+        }
+    }
+
+    void UpdatePlayer()
+    {
+        foreach (Player p in players)
+        {
+            if (p.inputMode != p.inputModeOld)
+            {
+                p.inputModeOld = p.inputMode;
+                p.horizontalInput = "Player_" + p.playerNum + "_" + p.inputMode.ToString() + "_Horizontal";
+                p.verticalInput = "Player_" + p.playerNum + "_" + p.inputMode.ToString() + "_Vertical";
+                p.button = "Player_" + p.playerNum + "_" + p.inputMode.ToString() + "_Button";
+            }
         }
     }
 }
 
-public enum InputMode{
-    Keyboard,
-    Mouse,
-    Controller
-}
+    public enum InputMode
+    {
+        Keyboard,
+        Mouse,
+        Controller
+    }
