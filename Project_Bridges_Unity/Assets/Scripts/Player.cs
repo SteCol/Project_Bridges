3860af5743e3047e9f51a5f140f92590f6b7016f
Stef Colruyt <stef.col@hotmail.com>
2017-04-30 22:11:17 +0200

    Added player generation and input assignment.

---
+++ Project_Bridges_Unity/Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Player  {
+
+    public string name;
+
+    [Header("Setup")]
+    public int playerNum;
+    public InputMode inputMode;
+    public InputMode inputModeOld;
+
+    [Header("Inputs")]
+    public string horizontalInput;
+    public string verticalInput;
+    public string button;
+
+    [Header("Blocks")]
+    public List<GameObject> blocks;
+
+    public Player(int _playerInt, InputMode _inputMode) {
+        name = "Player_" + _playerInt;
+        playerNum = _playerInt;
+        inputMode = _inputMode;
+        horizontalInput = "Player_" + playerNum + "_" + inputMode.ToString() + "_Horizontal";
+        verticalInput = "Player_" + playerNum + "_" + inputMode.ToString() + "_Vertical";
+        button = "Player_" + playerNum + "_" + inputMode.ToString() + "_Button";
+    }
+}
---
+++ Project_Bridges_Unity/Assets/Scripts/PlayerSetup.cs
@@ -0,0 +1,48 @@
+﻿//First with mouse, to get everything worked out without controllers.
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class PlayerSetup : MonoBehaviour {
+
+    public bool generate;
+    public bool update;
+
+    public int amountOfPlayers;
+
+    public List<Player> players;
+
+    void Start() {
+        
+    }
+
+    void Update() {
+        if (generate) {
+            players.Clear();
+            for (int i = 1; i <= amountOfPlayers; i++) {
+                players.Add(new Player(i, InputMode.Keyboard));
+            }
+            generate = false;
+        }
+
+        if (update) {
+            foreach (Player p in players) {
+                if (p.inputMode != p.inputModeOld) {
+                    p.inputModeOld = p.inputMode;
+                    p.horizontalInput = "Player_" + p.playerNum + "_" + p.inputMode.ToString() + "_Horizontal";
+                    p.verticalInput = "Player_" + p.playerNum + "_" + p.inputMode.ToString() + "_Vertical";
+                    p.button = "Player_" + p.playerNum + "_" + p.inputMode.ToString() + "_Button";
+                }
+            }
+            update = false;
+        }
+    }
+}
+
+public enum InputMode{
+    Keyboard,
+    Mouse,
+    Controller
+}
