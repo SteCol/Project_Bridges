81cb84e668d04f1a0a19100d8dd585f04aac77ce
Stef Colruyt <stef.col@hotmail.com>
2017-05-06 17:09:43 +0200

    Inproved full train animation.

--- Project_Bridges_Unity/Assets/Scripts/NPC/TrainAnimation.cs
+++ Project_Bridges_Unity/Assets/Scripts/NPC/TrainAnimation.cs
@@ -6,17 +6,31 @@ public class TrainAnimation : MonoBehaviour {
 
     public Animator trainAnimator;
 
+    public List<GameObject> carts;
+
     void Start() {
         StartCoroutine(iBump());
     }
 
     public IEnumerator iBump() {
-        float wait = Random.Range(0.5f,2.0f);
+        float wait = Random.Range(2.0f,5.0f);
         yield return new WaitForSeconds(wait);
         trainAnimator.SetBool("Bump", true);
+        for (int i = 0; i < carts.Count; i++)
+        {
+            StartCoroutine(iBumpCart(carts[i], i));
+        }
         yield return new WaitForEndOfFrame();
         trainAnimator.SetBool("Bump", false);
         StartCoroutine(iBump());
     }
-	
+
+    public IEnumerator iBumpCart(GameObject _g, float _i) {
+        float wait = (_i + 1) * 0.3f;
+        yield return new WaitForSeconds(wait);
+        _g.GetComponent<Animator>().SetBool("Bump", true);
+        yield return new WaitForEndOfFrame();
+        _g.GetComponent<Animator>().SetBool("Bump", false);
+    }
+
 }
